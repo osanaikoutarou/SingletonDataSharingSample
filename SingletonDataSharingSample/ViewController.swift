cbcd61c8d5e6c35e280f3b6c4d95ce1a6edb5d43
osanaikoutarou <osanaikoutarou+github@gmail.com>
2018-11-17 09:02:41 +0900

    Initial Commit

---
+++ SingletonDataSharingSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingletonDataSharingSample
+//
+//  Created by 長内幸太郎 on 2018/11/17.
+//  Copyright © 2018年 長内幸太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
