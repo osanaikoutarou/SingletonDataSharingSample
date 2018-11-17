e31f86bcd8bd113274609aa5e06c66f2727b78f7
osanaikoutarou <osanaikoutarou+github@gmail.com>
2018-11-17 13:53:45 +0900

    first commit

---
+++ SingletonDataSharingSample/MyModels.swift
@@ -0,0 +1,9 @@
+//
+//  MyModel.swift
+//  SingletonDataSharingSample
+//
+//  Created by 長内幸太郎 on 2018/11/17.
+//  Copyright © 2018年 長内幸太郎. All rights reserved.
+//
+
+import Foundation
---
+++ SingletonDataSharingSample/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  SingletonDataSharingSample
+//
+//  Created by 長内幸太郎 on 2018/11/17.
+//  Copyright © 2018年 長内幸太郎. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
